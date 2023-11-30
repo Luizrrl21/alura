1b7d6fea3e305e505f840901a86f0bfea8c3f0fa
Luiz Roberto <145810784+Luizrrl21@users.noreply.github.com>
2023-11-30 11:02:18 -0300

    Delete logica-js-projeto_inicial directory

--- logica-js-projeto_inicial/app.js
+++
@@ -1,8 +0,0 @@
-alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 29;
-let chute = prompt('Escolha um número entre 1 e 30');
-
-if (numeroSecreto == chute) {
-    console.log('Acertou')
-}
-
